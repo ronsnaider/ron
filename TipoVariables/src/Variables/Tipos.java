73ed459bcb2a67ca790085df5fd58302cab180c0
ronsnaider <109690769+ronsnaider@users.noreply.github.com>
2022-07-20 20:01:11 +0200

    Add files via upload

---
+++ TipoVariables/src/Variables/Tipos.java
@@ -0,0 +1,38 @@
+package Variables;
+
+public class Tipos {
+    public static void main(String[] args) {
+        // 1. enteros
+        byte var1 = 10;
+        System.out.println("resultado var1 " + var1);
+        short var2 = 20;
+        System.out.println("resultado var2 "+ var2);
+        int var3 = 30;
+        System.out.println("resultado var3"+ var3);
+        long var4 = 40;
+        System.out.println("resultado var4 "+ var4);
+
+        // 1.1 decimales
+        float var5 = 0.5f;
+        System.out.println("resultado var5 "+ var5);
+        double var6 = 5.5d;
+        System.out.println("resultado var6 "+ var6);
+        // 1.2 boleano
+        boolean var7 = false;
+        System.out.println("resultado var7 " + var7);
+        boolean var8 = true;
+        System.out.println("resultado var8 "+ var8);
+        // texto
+        char var9 = 'a';
+        System.out.println("resultado var9 "+ var9);
+        String var10 = "holla";
+        System.out.println("resultado var10 "+ var10);
+
+
+
+
+
+        }
+
+    }
+
